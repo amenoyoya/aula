3bfd69a734cbd9114787cbabb421f107b91cdf5e
amenoyoya <mtyswe3@gmail.com>
2021-10-02 23:42:32 +0900

    🎉 release: 1.0.0

--- src/build.bat
+++ src/build.bat
@@ -4,7 +4,8 @@ cd %~dp0
 call ..\vcvars.bat
 
 %compile% aula.cpp
-@link.exe /ERRORREPORT:NONE /NOLOGO /MACHINE:X86 /OUT:aula.exe aula.obj
+rc.exe /NOLOGO aula.rc
+@link.exe /ERRORREPORT:NONE /NOLOGO /MACHINE:X86 /OUT:aula.exe aula.obj aula.res
 
-del aula.exp aula.lib aula.obj
 move aula.exe ..\dist\bin\x86\
+del aula.exp aula.lib aula.obj aula.res
