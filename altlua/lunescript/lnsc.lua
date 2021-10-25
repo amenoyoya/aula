d1f3ca7a7b45e6e35a724d558a832c3f0a31f3b4
amenoyoya <mtyswe3@gmail.com>
2021-10-26 03:03:18 +0900

    🍱 add: lunescript as resource package

--- altlua/lunescript/lnsc.lua
+++ altlua/lunescript/lnsc.lua
@@ -1,16 +1,3 @@
-#! /usr/bin/env lua5.3
-
-function table.slice(self, from, to)
-    local new = {}
-    from = from or 1
-    to = to or #self
-    for i = 1, to - from + 1 do
-        new[i] = self[i + from - 1]
-    end
-    return new
-end
-
 arg = table.slice(os.argv, 2)
 table.insert( arg, 1, "" )
 require( 'lune.base.base' )
-
